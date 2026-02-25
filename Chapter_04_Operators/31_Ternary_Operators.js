585cfb56b88049cad2afa3b7a93057cc5c495eec
kireet28 <kireet2828@gmail.com>
2026-02-25 17:31:57 +0530

    Add Chapter 04 - Operators (arithmetic, assignment, comparison, logical, string, ternary, type, nullish coalescing)

---
+++ Chapter_04_Operators/31_Ternary_Operators.js
@@ -0,0 +1,10 @@
+let age = 17;
+
+let is_pramod_will_go_to_goa = age > 18 ? "Yes, let go goa!" : "No you are minor, Not going";
+console.log(is_pramod_will_go_to_goa)
+// Ternary = condition ? "true result" : "false result";
+
+let age_vote = 20;
+
+let is_eligible_for_vote = age_vote >= 18 ? "Yes, eligible for vote" : "No, not eligible for vote"
+console.log(is_eligible_for_vote);
---
+++ Chapter_04_Operators/32_Null_Optinal_Value.js
@@ -0,0 +1,12 @@
+let amul = null;
+let val = amul ?? "nandani milk";
+let val2 = null ?? "default";   // "default" (?? returns right side if left is null/undefined)
+
+// Modern JS - Typescript.
+
+val = "which milk? -> " + val;
+console.log(val)
+// very useful in test data handling.
+
+val = "which milk? ->" + val2;
+console.log(val2);
